34fb4da439d91fcf8d933e6b015036912bf2d358
Yasirooz <testwebsite1244@gmail.com>
2022-09-28 22:41:17 +0330

    change admin settings

--- Student/admin.py
+++ Student/admin.py
@@ -1,1 +1,34 @@
 from django.contrib import admin
+from Student.models import ClassRoom, Student, Attendance
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+
+    def shift(self, obj):
+        return obj.class_room
+    shift.short_description = 'shift'
+
+    list_display = ['first_name', 'last_name', 'father_name', 'id_code', 'level', 'shift']
+    list_filter = ['first_name', 'last_name', 'id_code']
+    
+
+@admin.register(ClassRoom)
+class ClassRoomAdmin(admin.ModelAdmin):
+    
+    def shift(self, obj):
+        return obj.class_room
+    shift.short_description = 'shift'
+
+    list_display = ['name', 'shift']
+
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    def student(self, obj):
+        return obj.student
+    
+    student.short_description = 'student'
+
+    list_display = ['student', 'attendance', 'date_attendance']
+    
