c590962caa7d97fff6fc9e24fa5c2fe27cb880c3
Yasirooz <testwebsite1244@gmail.com>
2022-09-29 06:58:34 +0330

    changing

--- Account/forms.py
+++ Account/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class LoginForm(forms.Form):
     
-    username = forms.CharField(widget=forms.TextInput({'class':'form-control', 'placeholder':'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput({'class':'form-control', 'placeholder':'Password'}))
+    username = forms.CharField(widget=forms.TextInput({'class':'form-control', 'placeholder':'نام کاربری'}))
+    password = forms.CharField(widget=forms.PasswordInput({'class':'form-control', 'placeholder':'رمز عبور'}))
     
