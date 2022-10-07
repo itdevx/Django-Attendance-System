5cab7b2c247908afb584e1fff201e2a07d638321
Yasirooz <testwebsite1244@gmail.com>
2022-10-07 19:11:09 +0330

    update

--- Student/urls.py
+++ Student/urls.py
@@ -45,38 +45,3 @@ urlpatterns = [
         'created/create-reshte/', views.CreateReshteView.as_view(), name='create-reshte'
     ),
 ]
-
-
-
-
-
-
-# urlpatterns = [
-#     path(
-#         '', views.IndexView.as_view(), name='index',
-#     ),
-#     path(
-#         'class-room/<shift>/<class_name>', views.ClassRoomView.as_view(), name='class_room'
-#     ),
-#     path(   
-#         'attendance/<class_name>', views.AttendanceView.as_view(), name='attendance'
-#     ),
-#     path(
-#         'create-student', views.CreateStudentView.as_view(), name='create-student'
-#     ),
-#     path(
-#         'student-info/<last_name>/<id_code>', views.StudentInfo.as_view(), name='student-info'
-#     ),
-#     # path(
-#     #     'student-edit/<int:id_code>', views.StudentEdit.as_view(), name='student-edit'
-#     # ),
-#     path(
-#         'student-edit/<id_code>', views.student_edit, name='student-edit'
-#     ),
-#     path(
-#         'attendance-test/<class_name>', views.AttendanceViewTest.as_view(), name='a-test'
-#     ),
-#     path(
-#         'wallet', views.WalletView.as_view(), name='wallet'
-#     )
-# ]
