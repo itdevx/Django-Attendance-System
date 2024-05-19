4d3389025398b9990b9e6df7601ba1dfa87fb984
itdevx <testwebsite1244@gmail.com>
2024-05-19 09:33:20 +0330

    add new export pdf

--- Student/views.py
+++ Student/views.py
@@ -22,6 +22,12 @@ from xhtml2pdf import pisa
 import pandas as pd
 from io import StringIO
 from django.contrib import messages
+import os
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
 
 
 class IndexView(LoginRequiredMixin, generic.View):
@@ -401,16 +407,48 @@ def export_csv(request, id_code):
     write = csv.writer(response)
     write.writerow(['نام و نام خانوادگی', 'نام پدر', 'شماره ملی', 'شماره کلاس', 'شیفت', 'پایه تحصیلی', 'رشته تحصیلی'])
     for s in student:
-        write.writerow([s.full_name, s.father_name, s.id_code, s.class_id.number, s.reshte, s.class_id.shift, s.level, s.reshte])
+        write.writerow([s.full_name, s.father_name, s.id_code, s.class_id.number, s.class_id.shift, s.reshte,  s.level, s.reshte])
     return response
+
+
+def link_callback(uri, rel):
+        """
+        Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+        resources
+        """
+        result = finders.find(uri)
+        if result:
+                if not isinstance(result, (list, tuple)):
+                        result = [result]
+                result = list(os.path.realpath(path) for path in result)
+                path=result[0]
+        else:
+                sUrl = settings.STATIC_URL        # Typically /static/
+                sRoot = settings.STATIC_ROOT      # Typically /home/userX/project_static/
+                mUrl = settings.MEDIA_URL         # Typically /media/
+                mRoot = settings.MEDIA_ROOT       # Typically /home/userX/project_static/media/
+
+                if uri.startswith(mUrl):
+                        path = os.path.join(mRoot, uri.replace(mUrl, ""))
+                elif uri.startswith(sUrl):
+                        path = os.path.join(sRoot, uri.replace(sUrl, ""))
+                else:
+                        return uri
+
+        # make sure that file exists
+        if not os.path.isfile(path):
+                raise RuntimeError(
+                        'media URI must start with %s or %s' % (sUrl, mUrl)
+                )
+        return path
 
 
 def render_to_pdf(template_src, context={}):
     template = get_template(template_src)
     html = template.render(context)
     result = BytesIO()
-    # pdf = pisa.pisaDocument(BytesIO(html.encode('UTF-8')), result)
-    pdf = pisa.CreatePDF(BytesIO(html.encode('utf-8')), dest=result)
+    pdf = pisa.pisaDocument(BytesIO(html.encode('UTF-8')), result)
+    # pdf = pisa.CreatePDF(BytesIO(html.encode('utf-8')), dest=result)
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
     return None
@@ -420,7 +458,6 @@ def export_pdf(request, id_code):
     # template = get_template('pdf-output.html')
     student = models.Student.objects.filter(id_code=id_code).values()
     df = pd.DataFrame(student)
-    print(df)
     context = {
         'student': student
     }
